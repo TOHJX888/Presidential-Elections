10558c5ca6889d84c824815c5c77d20766c836fe
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-08-26 09:20:50 +0800

    Initial Commit

---
+++ Presidential ElectionsUITests/Presidential_ElectionsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Presidential_ElectionsUITestsLaunchTests.swift
+//  Presidential ElectionsUITests
+//
+//  Created by RGS on 26/8/23.
+//
+
+import XCTest
+
+final class Presidential_ElectionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
