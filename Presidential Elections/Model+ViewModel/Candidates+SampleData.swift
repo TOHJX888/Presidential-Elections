c33e907f51d2196e7f00c4282b58bcd4556dfa5b
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-08-26 10:36:27 +0800

    added CandidateDetailView

--- Presidential Elections/Model+ViewModel/Candidates+SampleData.swift
+++ Presidential Elections/Model+ViewModel/Candidates+SampleData.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension Candidate {
+    
+    static let sampleCandidates = [
+        
+        Candidate(name: "Vincent", age: 55, numberOfCats: 20, amountOfHair: 0.5),
+        Candidate(name: "Olive", age: 30, numberOfCats: 1, amountOfHair: 0),
+        Candidate(name: "Washington", age: 70, numberOfCats: 0, amountOfHair: 50)
+        
+    ]
+}
