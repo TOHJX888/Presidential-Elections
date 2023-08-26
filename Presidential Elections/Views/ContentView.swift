424caf2855aa5fce095f687bbba2b79b1603df6d
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-08-26 10:56:14 +0800

    added contentview

--- Presidential Elections/Views/ContentView.swift
+++ Presidential Elections/Views/ContentView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var candidateManager = CandidateManager()
+    
     var body: some View {
         NavigationStack {
-            List(Candidate.sampleCandidates){candidate in
-                
+            List($candidateManager.candidates) { $candidate in
+                NavigationLink {
+                    CandidateDetailView(candidate: $candidate)
+                } label: {
+                    HStack {
+                        Text(candidate.name)
+                        Spacer()
+                        Text("\(candidate.votes)")
+                    }
+                }
+            }
+            .navigationTitle("Candidates 2023")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        candidateManager.candidates = Candidate.sampleCandidates
+                    } label: {
+                        Label("Load sample data", systemImage: "clipboard")
+                    }
+                }
             }
         }
     }
